88d39d9847dffd21459cd59861a507527c9f4a3a
Ram Montilia Smithson <ramsmithson1@gmail.com>
2021-06-09 12:08:30 +0300

    fixing addNewPet function to upload an image to cloudinary and save all detailes in mongoDB

--- routs/petRouter.js
+++ routs/petRouter.js
@@ -1,24 +1,7 @@
 const express = require('express')
 const multer = require("multer");
 const router = express.Router()
-const cloudinary = require('cloudinary').v2;
-cloudinary.config({
-    cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
-    api_secret: process.env.api_secret
-});
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, './images')
-    },
-    filename: function(req, file, cb) {
-      console.log(file)
-      cb(null, file.originalname)
-    }
-  })
-
-const upload = multer({ storage });
-
+const fileUpload = multer({ dest: 'uploads/' })
 
 const { getPetById, deletePetById, updatePetById, getPets, addNewPet, adoptPet, returnPet, fosterPet } = require("../controllers/petController")
 
@@ -28,7 +11,7 @@ router.get("/:id", getPetById);
 
 router.delete("/:id", deletePetById);
 
-router.post("/", express.json(), upload.single("image"), addNewPet);
+router.post("/", fileUpload.single("image"), addNewPet);
 
 router.put("/:id", updatePetById);
 
--- server.js
+++ server.js
@@ -3,26 +3,14 @@ const app = express()
 const cors = require("cors")
 const petRouter = require("./routs/petRouter");
 const userRouter = require("./routs/usersRouter")
-const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const port = process.env.port || 6000
-const url = process.env.MONGO_CONNECTION_STRING ;
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://Ram:<password>@cluster0.ezysc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-// });
 
 app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
-// app.use(bodyParser.json}
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use('/api/pets', petRouter)
 app.use('/api/users', userRouter)
 
